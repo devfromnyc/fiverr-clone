48992a4846c1500652a924275ae628610a5f265b
devfromnyc <devfromnyc@gmail.com>
2024-01-11 07:32:11 -0500

    initialize app

---
+++ README.md
@@ -0,0 +1,3 @@
+# FIVERR Clone
+
+LINK: TBD
---
+++ api/models/user.model.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: false,
+    },
+    phone: {
+      type: String,
+      required: false,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: false,
+    },
+    isSeller: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("User", UserSchema);

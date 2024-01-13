5c36deb7c20d0401b2676dd8aff74bbcc8c3ec0c
devfromnyc <devfromnyc@gmail.com>
2024-01-13 07:59:18 -0500

    feat: implemented new routes and jwt tokens

--- api/server.js
+++ api/server.js
@@ -1,7 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import authRoute from "./routes/auth.route.js";
 import userRoute from "./routes/user.route.js";
+import gigRoute from "./routes/gig.route.js";
+import orderRoute from "./routes/order.route.js";
+import conversationRoute from "./routes/conversation.route.js";
+import messageRoute from "./routes/message.route.js";
+import reviewRoute from "./routes/review.route.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 dotenv.config();
@@ -15,8 +22,20 @@ const connect = async () => {
   }
 };
 
-app.use("/api/user", userRoute);
+// Middlewares
+app.use(express.json());
+app.use(cookieParser());
 
+// Api Routes
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/gigs", gigRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/coversations", conversationRoute);
+app.use("/api/messages", messageRoute);
+app.use("/api/reviews", reviewRoute);
+
+// Server Listener
 app.listen(8800, () => {
   connect();
   console.log("Backend Server Is Running");

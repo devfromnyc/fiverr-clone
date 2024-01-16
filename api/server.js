470c0aee9c88e30caaf379c54c18eca281deea1f
devfromnyc <devfromnyc@gmail.com>
2024-01-16 15:27:34 -0500

    feat: established login from frontend

--- api/server.js
+++ api/server.js
@@ -9,6 +9,7 @@ import conversationRoute from "./routes/conversation.route.js";
 import messageRoute from "./routes/message.route.js";
 import reviewRoute from "./routes/review.route.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
@@ -23,6 +24,7 @@ const connect = async () => {
 };
 
 // Middlewares
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,10 +1,51 @@
-import React from "react"
-import "./Login.scss"
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Login.scss";
+import newRequest from "../../utils/newRequest.js";
 
 function Login() {
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await newRequest.post("/api/auth/login", {
+        username,
+        password,
+      });
+      localStorage.setItem("currentUser", JSON.stringify(res.data));
+      navigate("/");
+    } catch (err) {
+      setError(err.response.data);
+    }
+  };
   return (
-    <div>Login</div>
-  )
+    <div className="login">
+      <form onSubmit={handleSubmit}>
+        <h1>Sign In</h1>
+        <label htmlFor="username">Username</label>
+        <input
+          name="username"
+          type="text"
+          placeholder="johndoe"
+          onChange={(e) => setUserName(e.target.value)}
+        />
+
+        <label htmlFor="password">Password</label>
+        <input
+          name="password"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+        {error && error}
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;

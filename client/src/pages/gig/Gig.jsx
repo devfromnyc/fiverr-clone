5eb3ea88aa53f4fcf2cbaf7c9850e675f0a61e40
devfromnyc <devfromnyc@gmail.com>
2024-01-24 06:49:33 -0500

    feat: created user reviews on frontend

--- client/src/pages/gig/Gig.jsx
+++ client/src/pages/gig/Gig.jsx
@@ -15,6 +15,8 @@ function Gig() {
       }),
   });
 
+  const userId = data?.userId;
+
   const {
     isLoading: isLoadingUser,
     error: errorUser,
@@ -22,9 +24,10 @@ function Gig() {
   } = useQuery({
     queryKey: ["user"],
     queryFn: () =>
-      newRequest.get(`/users/${data.userId}`).then((res) => {
+      newRequest.get(`/users/${userId}`).then((res) => {
         return res.data;
       }),
+    enabled: !!userId,
   });
 
   return (
@@ -125,140 +128,11 @@ function Gig() {
                 </div>
               </div>
             )}
-            <div className="reviews">
-              <h2>Reviews</h2>
-              <div className="item">
-                <div className="user">
-                  <img
-                    className="pp"
-                    src="https://images.pexels.com/photos/839586/pexels-photo-839586.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                    alt=""
-                  />
-                  <div className="info">
-                    <span>Garner David</span>
-                    <div className="country">
-                      <img
-                        src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1fa-1f1f8.png"
-                        alt=""
-                      />
-                      <span>United States</span>
-                    </div>
-                  </div>
-                </div>
-                <div className="stars">
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <span>5</span>
-                </div>
-                <p>
-                  I just want to say that art_with_ai was the first, and after
-                  this, the only artist Ill be using on Fiverr. Communication
-                  was amazing, each and every day he sent me images that I was
-                  free to request changes to. They listened, understood, and
-                  delivered above and beyond my expectations. I absolutely
-                  recommend this gig, and know already that Ill be using it
-                  again very very soon
-                </p>
-                <div className="helpful">
-                  <span>Helpful?</span>
-                  <img src="/img/like.png" alt="" />
-                  <span>Yes</span>
-                  <img src="/img/dislike.png" alt="" />
-                  <span>No</span>
-                </div>
-              </div>
-              <hr />
-              <div className="item">
-                <div className="user">
-                  <img
-                    className="pp"
-                    src="https://images.pexels.com/photos/4124367/pexels-photo-4124367.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                    alt=""
-                  />
-                  <div className="info">
-                    <span>Sidney Owen</span>
-                    <div className="country">
-                      <img
-                        src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1e9-1f1ea.png"
-                        alt=""
-                      />
-                      <span>Germany</span>
-                    </div>
-                  </div>
-                </div>
-                <div className="stars">
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <span>5</span>
-                </div>
-                <p>
-                  The designer took my photo for my book cover to the next
-                  level! Professionalism and ease of working with designer along
-                  with punctuality is above industry standards!! Whatever your
-                  project is, you need this designer!
-                </p>
-                <div className="helpful">
-                  <span>Helpful?</span>
-                  <img src="/img/like.png" alt="" />
-                  <span>Yes</span>
-                  <img src="/img/dislike.png" alt="" />
-                  <span>No</span>
-                </div>
-              </div>
-              <hr />
-              <div className="item">
-                <div className="user">
-                  <img
-                    className="pp"
-                    src="https://images.pexels.com/photos/842980/pexels-photo-842980.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                    alt=""
-                  />
-                  <div className="info">
-                    <span>Lyle Giles </span>
-                    <div className="country">
-                      <img
-                        src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1fa-1f1f8.png"
-                        alt=""
-                      />
-                      <span>United States</span>
-                    </div>
-                  </div>
-                </div>
-                <div className="stars">
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <span>5</span>
-                </div>
-                <p>
-                  Amazing work! Communication was amazing, each and every day he
-                  sent me images that I was free to request changes to. They
-                  listened, understood, and delivered above and beyond my
-                  expectations. I absolutely recommend this gig, and know
-                  already that Ill be using it again very very soon
-                </p>
-                <div className="helpful">
-                  <span>Helpful?</span>
-                  <img src="/img/like.png" alt="" />
-                  <span>Yes</span>
-                  <img src="/img/dislike.png" alt="" />
-                  <span>No</span>
-                </div>
-              </div>
-            </div>
           </div>
           <div className="right">
             <div className="price">
               <h3>{data.shortTitle}</h3>
-              <h2>{data.price}</h2>
+              <h2>${data.price}</h2>
             </div>
             <p>{data.shortDesc}</p>
             <div className="details">
